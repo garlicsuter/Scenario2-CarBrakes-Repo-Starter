1351787d17a49f052764bd333aeacc1b86cbb5c4
Garlicsuter <markasuter@gmail.com>
2023-03-24 18:37:09 -0400

    Instructions Added. Scripts Ready to attach

--- Assets/Scripts/tire.cs
+++ Assets/Scripts/tire.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class tire : MonoBehaviour
+public class Tire : MonoBehaviour
 {
     public Rigidbody rb;
     public GameManager GM;
